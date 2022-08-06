808ef775b9a5f3622d9372a73e01315975451362
Mitsukai74 <ansaanva@gmail.com>
2022-08-06 02:23:36 -0500

    terminando controller para la creacion de producto

--- controlador/crearProducto_controller.js
+++ controlador/crearProducto_controller.js
@@ -1,2 +1,17 @@
 import { productos_servicios } from "../servicios/producto_servicios";
 
+const form = document.querySelector("[data-form]");
+form.addEventListener("submit",(evento) => {
+    evento.preventDefault();
+    const imagen = document.querySelector("[data-img]").value
+    const nombre = document.querySelector("[data-nombre]").value
+    const descripcion = document.querySelector("[data-descrip]").value
+
+    productos_servicios.crearProduto(imagen,nombre,descripcion).then(respuesta => {
+        window.location.href = "/index.html"
+        alert("Producto creado")
+        console.log(respuesta);
+    }).catch(err => {
+        console.log(err)
+    })
+});
